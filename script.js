a533caab7845f4c9b941290011f830fce0fb4d49
jackrtl-exe <pedrosillvaq.96@gmail.com>
2023-05-25 21:36:37 -0300

    finished

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
-function enviarNome(){
-    let nome = document.getElementById("nome-recebido").value
-    document.getElementById("nome").innerHTML = nome
-}
-
-function calcularMedia(){
-    let n1 = Number.getElementById("Digite a primeira nota: ").value
-    let n2 = Number.getElementById("Digite a segunda nota: ").value
-    let n3 = Number.getElementById("Digite a terceira nota: ").value
-    let media = (n1 + n2 + n3)/3
-    document.getElementById("resultado").innerHTML = media
+function nomeMedia() {
+    let nome  = document.getElementById("nome-recebido").value;
+    let n1    = Number(document.getElementById("notas-recebidas").value);
+    let n2    = Number(document.getElementById("notas-recebidas").value);
+    let n3    = Number(document.getElementById("notas-recebidas").value);
+    let media = (n1 + n2 + n3)/3;
 
     // if else
-    if (media >= 6){
-        let resultado = "Aprovado!"
-        document.getElementById("geral").innerHTML = resultado
+    if (media >= 6) {
+        let resultado = "APROVADO!";
+        document.getElementById("nome").innerHTML = `O aluno ${nome} com média ${media} está ${resultado}`
     }
-
-    else{
-        let resultado = "Reprovado!"
-        document.getElementById("geral").innerHTML = resultado
+    else {
+        let resultado = "REPROVADO!";
+        document.getElementById("nome").innerHTML = `O aluno ${nome} com média ${media} está ${resultado}`
     }
 }
