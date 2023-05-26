bd7a732e70e4e667e0969067c4e0ed37520460ae
jackrtl-exe <pedrosillvaq.96@gmail.com>
2023-05-26 08:16:58 -0300

    finished (fixed media)

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function nomeMedia() {
     let nome  = document.getElementById("nome-recebido").value;
     let n1    = Number(document.getElementById("notas-recebidas").value);
-    let n2    = Number(document.getElementById("notas-recebidas").value);
-    let n3    = Number(document.getElementById("notas-recebidas").value);
-    let media = (n1 + n2 + n3)/3;
+    let n2    = Number(document.getElementById("notas-recebidas2").value);
+    let n3    = Number(document.getElementById("notas-recebidas3").value);
+    let media = Number((n1 + n2 + n3)/3);
 
     // if else
     if (media >= 6) {
         let resultado = "APROVADO!";
-        document.getElementById("nome").innerHTML = `O aluno ${nome} com média ${media} está ${resultado}`
+        document.getElementById("nome").innerHTML = `O aluno ${nome} com média ${media.toFixed(2)} está ${resultado}`
     }
     else {
         let resultado = "REPROVADO!";
-        document.getElementById("nome").innerHTML = `O aluno ${nome} com média ${media} está ${resultado}`
+        document.getElementById("nome").innerHTML = `O aluno ${nome} com média ${media.toFixed(2)} está ${resultado}`
     }
 }
